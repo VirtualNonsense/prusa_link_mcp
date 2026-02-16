25e651189d71b764fc972ae680926b551e572c0e
virtualnonsense <virtualnonsense@mailbox.org>
2026-02-16 09:32:25 +0100

    added options for easy config overwrites

--- src/prusa_mcp/_cli.py
+++ src/prusa_mcp/_cli.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 from pathlib import Path
+from typing import Optional
 
 import click
 
@@ -12,19 +13,24 @@ def prusa_mcp() -> None:
     pass
 
 @prusa_mcp.command()
-def serve() -> None:
+@click.option("--host", type=str, help="Hostname or IP address")
+@click.option("--port", type=int, help="Port number")
+def serve(host: Optional[str] = None, port: Optional[int] = None) -> None:
     current_path = Path(__file__).parent.absolute()
     app = current_path.joinpath("app.py")
     settings = get_mcpo_settings()
     python = Path(sys.executable)
     mcpo = python.parent.joinpath("mcpo")
+
+    host = host or settings.mcpo_ip
+    port = port or settings.mcpo_port
     subprocess.call([
         python,
         mcpo,
         "--host",
-        settings.mcpo_ip,
+        host,
         "--port",
-        str(settings.mcpo_port),
+        str(port),
         "--",
         python,
         str(app)
